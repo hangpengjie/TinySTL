4e72fc836aeece0e718ba1b1f66f00949d184043
hangpengjie <596593218@qq.com>
2024-11-12 17:48:10 +0800

    Add multiple vector tests to enhance coverage and functionality

--- Test/vector.h
+++ Test/vector.h
@@ -12,12 +12,7 @@ struct A{
     }
 };
 
-struct B{
-    
-    B(){
-        std::cout << "B()" << std::endl;
-    }
-};
+
 void test_1(){
     hstl::vector<int> v;
     assert(v.size() == 16);
@@ -42,4 +37,98 @@ void test_4(){
     assert(v.size() == 10);
     assert(v.capacity() == 16);
     std::cout << "vector test 4 passed" << std::endl;    
+}
+
+void test_5(){
+    A a;
+    a.x =1;
+    hstl::vector<A> v(10,a);
+    assert(v.size() == 10);
+    assert(v.capacity() == 16);
+    std::cout << "vector test 5 passed" << std::endl;    
+}
+
+void test_6(){
+    hstl::vector<int> a(17);
+    hstl::vector<int> b(a);
+    assert(a.size() == b.size());
+    assert(a.capacity() == b.capacity());
+    assert(b.size() == 17);
+    std::cout << "vector test 6 passed" << std::endl;
+}
+
+void test_7(){
+    hstl::vector<int> a(17);
+    hstl::vector<int> b(std::move(a));
+    assert(b.size() == 17);
+    assert(a.size() == 0);
+    assert(a.capacity() == 0);
+    
+    std::cout << "vector test 7 passed" << std::endl;
+}
+
+void test_8(){
+    hstl::vector<int> v({0,1,2,3,4,5,6,7,8,9,10});
+    for(int i = 0; i < 11; i++){
+        assert(v[i] == i);
+    }
+    std::cout << "vector test 8 passed" << std::endl;
+}
+void test_9(){
+    hstl::vector<int> v({0,1,2,3,4,5,6,7,8,9,10});
+    for(int i = 0; i < 11; i++){
+        v[i] += 1;
+        assert(v[i] == i + 1);
+    }
+    std::cout << "vector test 9 passed" << std::endl;
+}
+
+void test_10(){
+    hstl::vector<int> v({0,1,2,3,4,5,6,7,8,9,10});
+    v.~vector();
+    std::cout << "vector test 10 passed" << std::endl;
+}
+
+struct B{
+    ~B(){
+        std::cout << "~B()" << std::endl;
+    }
+};
+void test_11(){
+    int n = 17;
+    hstl::vector<B> v(n);
+    std::cout << "Allocated " << n * sizeof(B) << " bytes" << std::endl;
+    v.~vector();
+    std::cout << "Deallocated " << n * sizeof(B) << " bytes" << std::endl;
+    std::cout << "vector test 11 passed" << std::endl;
+}
+
+struct C{
+    int x;
+    C(int x):x(x){
+        
+    }
+    C(const C& other){
+        x = other.x;
+        std::cout << "C(const C&)" << std::endl;
+    }
+};
+void test_12(){
+    hstl::vector<C> v = {C(0), C(1), C(2)};
+    hstl::vector<C> v2 = std::move(v);
+    assert(v2[0].x == 0);
+    assert(v2[1].x == 1);
+    assert(v2[2].x == 2);
+    
+    std::cout << "vector test 12 passed" << std::endl;
+}
+
+void test_13(){
+    hstl::vector<int> v = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    int a = 16;
+    v.push_back(a);
+    assert(v.capacity() == 24);
+    assert(v.size() == 17);
+    assert(v[16] == 16);
+    std::cout << "vector test 13 passed" << std::endl;
 }
--- TinySTL/vector.h
+++ TinySTL/vector.h
@@ -3,6 +3,7 @@
 
 #include <cstddef>
 #include <iostream>
+#include <cassert>
 #include "allocator.h"
 #include "uninitialized.h"
 
@@ -36,36 +37,39 @@ private:
 public:
     vector() noexcept;
     explicit vector(size_type n);
-    // vector(size_type n, const value_type& value);
-    // vector(const vector& rhs);
-    // vector(vector&& rhs) noexcept;
-    // vector(std::initializer_list<value_type> ilist);
+    vector(size_type n, const value_type& value);
+    vector(const vector& rhs);
+    vector(vector&& rhs) noexcept;
+    vector(std::initializer_list<value_type> ilist);
     
-    // ~vector();
+    ~vector();
 
-    // vector& operator=(const vector& rhs);
-    // vector& operator=(vector&& rhs) noexcept;
-    // vector& operator=(std::initializer_list<value_type> ilist);
+    vector& operator=(const vector& rhs);
+    vector& operator=(vector&& rhs) noexcept;
+    vector& operator=(std::initializer_list<value_type> ilist);
 
 public:
-    // iterator begin() noexcept;
-    // const_iterator begin() const noexcept;
-    // iterator end() noexcept;
-    // const_iterator end() const noexcept;
+    iterator begin() noexcept;
+    const_iterator begin() const noexcept;
+    iterator end() noexcept;
+    const_iterator end() const noexcept;
 
     size_type size() const noexcept;
+    size_type max_size() const noexcept;
     size_type capacity() const noexcept;
-    // bool empty() const noexcept;
+    reference operator[](size_type n);
+    const_reference operator[](size_type n) const;
+    bool empty() const noexcept;
 
     // void reserve(size_type new_cap);
     // void resize(size_type count);
     // void resize(size_type count, const value_type& value);
 
-    // void push_back(const value_type& value);
-    // void push_back(value_type&& value);
+    void push_back(const value_type& value);
+    void push_back(value_type&& value);
 
-    // template <typename... Args>
-    // void emplace_back(Args&&... args);
+    template <typename... Args>
+    void emplace_back(Args&&... args);
 
     // void pop_back();
 
@@ -75,14 +79,20 @@ public:
     // const_reference operator[](size_type n) const;
 private:
     void try_init() noexcept;
-    void fill_init(size_type init_size, const value_type& value);     // TODO
+    void fill_init(size_type init_size, const value_type& value);
     void init_place(size_type init_size, size_type init_cap);
+    
+    template <typename InputIterator>
+    void range_init(InputIterator first, InputIterator last);
+
+    void reallocate_insert(iterator position, const value_type& value);
+
+    size_type get_new_cap(size_type add_size) const noexcept;
 };
 
 template <typename T>
 vector<T>::vector() noexcept{
     try_init();
-    
 }
 
 template <typename T>
@@ -90,6 +100,82 @@ vector<T>::vector(size_type n){
     fill_init(n, value_type());
 }
 
+template <typename T>
+vector<T>::vector(size_type n, const value_type& value){
+    fill_init(n, value);
+}
+
+template <typename T>
+vector<T>::vector(const vector& rhs){
+    range_init(rhs.begin_, rhs.end_);
+}
+
+template <typename T>
+vector<T>::vector(vector&& rhs) noexcept : begin_(rhs.begin_), end_(rhs.end_), cap_(rhs.cap_){
+    rhs.begin_ = rhs.end_ = rhs.cap_ = nullptr;
+}
+
+template <typename T>
+vector<T>::vector(std::initializer_list<value_type> ilist){
+    range_init(ilist.begin(), ilist.end());
+}
+
+
+template <typename T>
+vector<T>::~vector(){
+    data_allocator::destroy(begin_, end_);
+    data_allocator::deallocate(begin_, cap_ - begin_);
+    begin_ = end_ = cap_ = nullptr;
+}
+
+template <typename T>
+vector<T>& vector<T>::operator=(const vector& rhs){
+    if(this != &rhs){
+        const size_type len = rhs.size();
+        if(len > capacity()){
+            vector tmp(rhs);
+            std::swap(begin_, tmp.begin_);
+            std::swap(end_, tmp.end_);
+            std::swap(cap_, tmp.cap_);
+        }else{
+            data_allocator::destroy(begin_, end_);
+            end_ = hstl::uninitialized_copy(rhs.begin(), rhs.end(), begin_);
+        }
+        
+    }
+    return *this;
+}
+
+template <typename T>
+vector<T>& vector<T>::operator=(vector&& rhs) noexcept{
+    if(this != &rhs){
+        data_allocator::destroy(begin_, end_);
+        data_allocator::deallocate(begin_, cap_ - begin_);
+        begin_ = rhs.begin_;
+        end_ = rhs.end_;
+        cap_ = rhs.cap_;
+        rhs.begin_ = rhs.end_ = rhs.cap_ = nullptr;
+    }
+    return *this;
+}
+
+
+template <typename T>
+typename vector<T>::iterator vector<T>::begin() noexcept{
+    return begin_;
+}
+template <typename T>
+typename vector<T>::const_iterator vector<T>::begin() const noexcept{
+    return begin_;
+}
+template <typename T>
+typename vector<T>::iterator vector<T>::end() noexcept{
+    return end_;
+}
+template <typename T>
+typename vector<T>::const_iterator vector<T>::end() const noexcept{
+    return end_;
+}
 
 template<typename T>
 typename vector<T>::size_type vector<T>::size() const noexcept{
@@ -97,8 +183,23 @@ typename vector<T>::size_type vector<T>::size() const noexcept{
 }
 
 template<typename T>
+typename vector<T>::size_type vector<T>::max_size() const noexcept{
+    return static_cast<size_type>(-1) / sizeof(T);
+}
+
+template<typename T>
 typename vector<T>::size_type vector<T>::capacity() const noexcept{
     return cap_ - begin_;
+}
+
+template<typename T>
+typename vector<T>::reference vector<T>::operator[](size_type n){
+    return *(begin_ + n);
+}
+
+template<typename T>
+typename vector<T>::const_reference vector<T>::operator[](size_type n) const{
+    return *(begin_ + n);
 }
 
 template <typename T>
@@ -109,6 +210,32 @@ void vector<T>::try_init() noexcept{
     }catch(...){
         begin_ = end_ = cap_ = nullptr;
     }
+}
+
+template <typename T>
+bool vector<T>::empty() const noexcept{
+    return begin_ == end_;
+}
+
+template <typename T>
+void vector<T>::push_back(const value_type& value){
+    if(end_ != cap_){
+        data_allocator::construct(end_++, value);
+    }else{
+        reallocate_insert(end_, value);
+    }
+}
+
+template <typename T>
+void vector<T>::push_back(value_type&& value){
+    // TODO
+}
+
+template <typename T>
+template <typename... Args>
+void vector<T>::emplace_back(Args&&... args){
+    // TODO
+    
 }
 
 
@@ -132,12 +259,45 @@ void vector<T>::init_place(size_type init_size, size_type init_cap){
     }
 }
 
+template <typename T>
+template <typename InputIterator>
+void vector<T>::range_init(InputIterator first, InputIterator last){
+    const size_type init_size = static_cast<size_type>(std::distance(first, last));
+    const size_type init_cap = std::max(static_cast<size_type>(16), init_size);
+    init_place(init_size, init_cap);
+    hstl::uninitialized_copy(first, last, begin_);
+}
 
+template <typename T>
+void vector<T>::reallocate_insert(iterator position, const value_type& value){
+    const size_type new_cap = get_new_cap(1);
+    iterator new_begin = data_allocator::allocate(new_cap);
+    iterator new_end = new_begin;
+    try{
+        new_end = hstl::uninitialized_move(begin_, position, new_begin);
+        data_allocator::construct(new_end++, value);
+        new_end = hstl::uninitialized_move(position, end_, new_end);
+    }catch(...){
+        data_allocator::deallocate(new_begin, new_cap);
+        throw;
+    }
+    hstl::destroy(begin_, end_);
+    data_allocator::deallocate(begin_, cap_ - begin_);
+    begin_ = new_begin;
+    end_ = new_end;
+    cap_ = begin_ + new_cap;
+}
 
-
-
-
-
+template <typename T>
+typename vector<T>::size_type vector<T>::get_new_cap(size_type add_size) const noexcept{
+    const size_type old_cap = capacity();
+    assert(old_cap  <= max_size() - add_size);
+    if (old_cap > max_size() - old_cap){
+        return old_cap + add_size > max_size() - 16 ? old_cap + add_size : old_cap + add_size + 16;
+    }
+    const size_type new_cap = old_cap == 0 ? std::max(add_size, static_cast<size_type>(16)) : std::max(old_cap + old_cap / 2, old_cap + add_size);
+    return new_cap;
+}
 
 }
 
