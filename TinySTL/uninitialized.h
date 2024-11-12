9afada90ed477d3aa68052450ae522521e810779
hangpengjie <596593218@qq.com>
2024-11-12 14:26:51 +0800

    Add uninitialized_copy function and enhance uninitialized_fill_n implementation

--- TinySTL/uninitialized.h
+++ TinySTL/uninitialized.h
@@ -5,7 +5,9 @@
 
 namespace hstl{
 
-
+/**
+ *  uninitialized_fill_n
+ */
 template <typename ForwardIterator, typename Size, typename T>
 ForwardIterator __uninitialized_fill_n_aux(ForwardIterator first, Size n, const T& x, std::true_type) {
     return std::fill_n(first, n, x);
@@ -39,6 +41,45 @@ template <typename ForwardIterator, typename Size, typename T>
 ForwardIterator uninitialized_fill_n(ForwardIterator first, Size n, const T& x){
     return __uninitialized_fill_n(first, n, x);
 }
+
+/**
+ * uninitialized_copy
+ */
+
+template <typename InputIterator, typename ForwardIterator>
+ForwardIterator __uninitialized_copy_aux(InputIterator first, InputIterator last, ForwardIterator result, std::true_type) {
+    return std::copy(first, last, result);
+}
+
+template <typename InputIterator, typename ForwardIterator>
+ForwardIterator __uninitialized_copy_aux(InputIterator first, InputIterator last, ForwardIterator result, std::false_type) {
+    ForwardIterator cur = result;
+    try {
+        for (; first != last; ++first, ++cur) {
+            hstl::construct(&*cur, *first);
+        }
+        return cur;
+    } catch (...) {
+        for (; result != cur; ++result) {
+            hstl::destroy(&*result);
+        }
+        throw;
+    }
+}
+
+template <typename InputIterator, typename ForwardIterator>
+ForwardIterator __uninitialized_copy(InputIterator first, InputIterator last, ForwardIterator result){
+    // std::is_pod is a type trait that checks if a type is a plain old data type.                        
+    // std::is_trivially_copy_assignable is a type trait that checks if a type is trivially copyable.
+    // std::is_pod is stricter thanstd::is_trivially_copy_assignable
+    return __uninitialized_copy_aux(first, last, result, typename std::is_pod<typename std::iterator_traits<InputIterator>::value_type>());
+}
+
+template <typename InputIterator, typename ForwardIterator>
+ForwardIterator uninitialized_copy(InputIterator first, InputIterator last, ForwardIterator result){
+    return __uninitialized_copy(first, last, result);
+}
+
 
 
 }
